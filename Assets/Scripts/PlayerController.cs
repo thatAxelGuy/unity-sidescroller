9c933a77bd88024ca00f790e4fa6e6207950389a
Axel Woermann <53866066+AxlMan31@users.noreply.github.com>
2025-09-08 16:31:00 +0200

    feat: add animations for jumping and gameover - prevent player from jumping after death

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,24 +1,30 @@
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-    [SerializeField] private float jumpForce = 10;
+    private Animator playerAnimator;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private float speedModifier = 1.5f;
     [SerializeField] private float gravityModifier;
     [SerializeField] bool isOnGround = true;
     public bool gameOver = false;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
+        playerAnimator.speed *= speedModifier;
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space) && isOnGround)
+        if (Input.GetKey(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnimator.SetTrigger("Jump_trig");
         }
     }
 
@@ -33,6 +39,8 @@ public class PlayerController : MonoBehaviour
         {
             gameOver = true;
             Debug.Log("Game Over!");
+            playerAnimator.SetBool("Death_b", true);
+            playerAnimator.SetInteger("DeathType_int", Random.Range(1, 4));
         }
         
     }
